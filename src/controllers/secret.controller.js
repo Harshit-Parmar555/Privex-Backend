f0bdb28d55182569174ebf8e44f330a42c266622
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-27 20:04:02 +0530

    feat: add get-secret controller to retrive message with uuid

--- src/controllers/secret.controller.js
+++ src/controllers/secret.controller.js
@@ -1,5 +1,5 @@
 import { Secret } from "../models/secret.model.js";
-import { encrypt } from "../utils/crypto.js";
+import { encrypt, decrypt } from "../utils/crypto.js";
 import { v4 as uuidv4 } from "uuid";
 
 export const createSecret = async (req, res) => {
@@ -24,14 +24,48 @@ export const createSecret = async (req, res) => {
 
     await newSecret.save();
 
+    const url = process.env.GET_SECRET_URL + `/${newSecret.uuid}`;
+
     return res.status(201).json({
       message: "Secret created successfully",
       uuid: newSecret.uuid,
+      url,
     });
   } catch (error) {
     return res.status(500).json({
       message: "Error creating secret",
       error: error.message,
     });
+  }
+};
+
+export const viewSecret = async (req, res) => {
+  try {
+    const { uuid } = req.params;
+    const secretDoc = await Secret.findOne({ uuid });
+
+    if (!secretDoc) {
+      return res.status(404).json({ message: "Secret not found" });
+    }
+    if (secretDoc.viewed) {
+      return res.status(410).json({ message: "Secret already viewed" });
+    }
+    if (new Date() > secretDoc.expireAt) {
+      return res.status(410).json({ message: "Secret expired" });
+    }
+
+    // Decrypt secret
+    const encrypted = JSON.parse(secretDoc.encryptedSecret);
+    const secretText = decrypt(encrypted);
+
+    // Mark as viewed
+    secretDoc.viewed = true;
+    await secretDoc.save();
+
+    return res.status(200).json({ secretText });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error retrieving secret", error: error.message });
   }
 };
