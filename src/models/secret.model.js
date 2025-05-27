f9ea0e07bef55d4474dada751dd8eed9966c66f1
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-27 19:03:07 +0530

    feat: add Secret model with Mongoose schema

---
+++ src/models/secret.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const secretSchema = new mongoose.Schema({
+  uuid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  encryptedSecret: {
+    type: String,
+    required: true,
+  },
+  expireAt: {
+    type: Date,
+    required: true,
+  },
+  viewed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Secret = mongoose.model("Secret", secretSchema);
