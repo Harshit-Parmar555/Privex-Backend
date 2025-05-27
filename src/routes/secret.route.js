5a8f7375b93438621d6fe7fc0d402ea373b13dfb
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-27 20:05:01 +0530

    feat: add get-secret route to retrieve secrets by uuid

--- src/routes/secret.route.js
+++ src/routes/secret.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createSecret } from "../controllers/secret.controller.js";
+import { createSecret, viewSecret } from "../controllers/secret.controller.js";
 
 const secretRouter = express.Router();
 
-secretRouter.post("/createSecret", createSecret);
+secretRouter.post("/create-secret", createSecret);
+secretRouter.get("/get-secret/:uuid", viewSecret);
 
 export { secretRouter };
