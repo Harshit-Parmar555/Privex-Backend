d5b2a9b72376f8634cf1076e08796e66cf9efa03
Harshit_Parmar <parmarharshit441@gmail.com>
2025-05-29 00:01:12 +0530

    feat: enable timestamps in secret model for created and updated tracking

--- src/models/secret.model.js
+++ src/models/secret.model.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const secretSchema = new mongoose.Schema({
-  uuid: {
-    type: String,
-    required: true,
-    unique: true,
+const secretSchema = new mongoose.Schema(
+  {
+    uuid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    encryptedSecret: {
+      type: String,
+      required: true,
+    },
+    expireAt: {
+      type: Date,
+      required: true,
+      expires: 0,
+    },
+    viewOnce: {
+      type: Boolean,
+      default: false,
+    },
   },
-  encryptedSecret: {
-    type: String,
-    required: true,
-  },
-  expireAt: {
-    type: Date,
-    required: true,
-    expires: 0,
-  },
-  viewOnce: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 export const Secret = mongoose.model("Secret", secretSchema);
